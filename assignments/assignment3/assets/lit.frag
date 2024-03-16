e611e07316592ab34a59e8d879889445c4eec273
Aidyn Short <109443741+Arshort@users.noreply.github.com>
2024-03-16 15:30:11 -0400

    assignment 3 started

---
+++ assignments/assignment3/assets/lit.frag
@@ -0,0 +1,111 @@
+#version 450
+out vec4 FragColor; //The color of this fragment
+in Surface{
+	vec3 WorldPos; //Vertex position in world space
+	vec3 WorldNormal; //Vertex normal in world space
+	vec2 TexCoord;
+}fs_in;
+
+
+in vec4 lightSpacePos;
+
+
+
+uniform sampler2D _MainTex; 
+uniform sampler2D _ShadowMap;
+
+uniform float minBias = 0.005;
+uniform float maxBias = 0.015;
+
+
+uniform vec3 _EyePos;
+uniform vec3 _LightDirection = vec3(0.0,-1.0,0.0);
+uniform vec3 _LightColor = vec3(1.0);
+uniform vec3 _AmbientColor = vec3(0.3,0.4,0.46);
+
+struct Material{
+	float Ka; //Ambient coefficient (0-1)
+	float Kd; //Diffuse coefficient (0-1)
+	float Ks; //Specular coefficient (0-1)
+	float Shininess; //Affects size of specular highlight
+};
+uniform Material _Material;
+
+
+float calcShadow(sampler2D shadowMap, vec4 lightSpacePos,float bias)
+{
+	vec3 sampleCoord = lightSpacePos.xyz / lightSpacePos.w;
+	sampleCoord = sampleCoord * 0.5 + 0.5;
+
+	
+
+	float myDepth = sampleCoord.z - bias;
+
+
+
+	float totalShadow = 0;
+	vec2 texelOffset = 1.0 / textureSize(_ShadowMap,0);
+
+	for(int y = -1; y <=1; y++)
+	{
+		for(int x = -1; x <=1; x++)
+		{
+			vec2 uv = sampleCoord.xy + vec2(x * texelOffset.x, y * texelOffset.y);
+			totalShadow+=step(texture(_ShadowMap,uv).r,myDepth);
+		}
+	}
+
+  totalShadow /= 9.0;
+
+	return totalShadow;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+
+
+	vec3 normal = normalize(fs_in.WorldNormal);
+
+
+	vec3 toLight = -_LightDirection;
+	float diffuseFactor = max(dot(normal,toLight),0.0);
+
+
+	vec3 toEye = normalize(_EyePos - fs_in.WorldPos);
+
+
+	vec3 h = normalize(toLight + toEye);
+	float specularFactor = pow(max(dot(normal,h),0.0),_Material.Shininess);
+
+
+	vec3 lightColor = (_Material.Kd * diffuseFactor + _Material.Ks * specularFactor) * _LightColor;
+
+
+
+	//lightColor+=_AmbientColor * _Material.Ka;
+
+	float bias = max(maxBias * (1.0 - dot(normal,toLight)),minBias);
+	float shadow = calcShadow(_ShadowMap, lightSpacePos, bias);
+
+	vec3 objectColor = texture(_MainTex,fs_in.TexCoord).rgb;
+
+	//vec3 light = lightColor * (1.0 -shadow);
+	
+	vec3 light = lightColor * (1.0-shadow);
+	light += _AmbientColor * _Material.Ka;
+	
+	
+	FragColor = vec4(objectColor * light,1.0);
+}
+
--- core/Aidyn/Framebuffer.h
+++ core/Aidyn/Framebuffer.h
@@ -4,7 +4,7 @@
 namespace aidyn {
 	struct Framebuffer {
 		unsigned int fbo;
-		unsigned int colorBuffer;
+		unsigned int colorBuffer[8];
 		unsigned int depthBuffer;
 		unsigned int width;
 		unsigned int height;
@@ -19,10 +19,10 @@ namespace aidyn {
 		glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer.fbo);
 
 		//create and bind colorbuffer
-		glGenTextures(1, &frameBuffer.colorBuffer);
-		glBindTexture(GL_TEXTURE_2D, frameBuffer.colorBuffer);
+		glGenTextures(1, &frameBuffer.colorBuffer[0]);
+		glBindTexture(GL_TEXTURE_2D, frameBuffer.colorBuffer[0]);
 		glTexStorage2D(GL_TEXTURE_2D, 1, colorFormat, width, height);
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, frameBuffer.colorBuffer, 0);
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, frameBuffer.colorBuffer[0], 0);
 
 
 
@@ -48,10 +48,10 @@ namespace aidyn {
 		glBindFramebuffer(GL_FRAMEBUFFER, frameBuffer.fbo);
 
 		//create and bind colorbuffer
-		glGenTextures(1, &frameBuffer.colorBuffer);
-		glBindTexture(GL_TEXTURE_2D, frameBuffer.colorBuffer);
+		glGenTextures(1, &frameBuffer.colorBuffer[0]);
+		glBindTexture(GL_TEXTURE_2D, frameBuffer.colorBuffer[0]);
 		glTexStorage2D(GL_TEXTURE_2D, 1, colorFormat, width, height);
-		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, frameBuffer.colorBuffer, 0);
+		glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, frameBuffer.colorBuffer[0], 0);
 
 
 
@@ -99,6 +99,76 @@ namespace aidyn {
 
 
 
+	Framebuffer createGBuffer(unsigned int width, unsigned int height) {
+		Framebuffer framebuffer;
+		framebuffer.width = width;
+		framebuffer.height = height;
+
+		glCreateFramebuffers(1, &framebuffer.fbo);
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+
+		int formats[3] = {
+			GL_RGB32F, //0 = World Position 
+			GL_RGB16F, //1 = World Normal
+			GL_RGB16F  //2 = Albedo
+		};
+ 	//Create 3 color textures
+		for (size_t i = 0; i < 3; i++)
+		{
+			glGenTextures(1, &framebuffer.colorBuffer[i]);
+			glBindTexture(GL_TEXTURE_2D, framebuffer.colorBuffer[i]);
+			glTexStorage2D(GL_TEXTURE_2D, 1, formats[i], width, height);
+
+
+
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+
+			glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, framebuffer.colorBuffer[i], 0);
+		}
+		//Explicitly tell OpenGL which color attachments we will draw to
+		const GLenum drawBuffers[3] = {
+				GL_COLOR_ATTACHMENT0, GL_COLOR_ATTACHMENT1, GL_COLOR_ATTACHMENT2
+		};
+		glDrawBuffers(3, drawBuffers);
+		//TODO: Add texture2D depth buffer
+		glGenTextures(1, &framebuffer.depthBuffer);
+		glBindTexture(GL_TEXTURE_2D, framebuffer.depthBuffer);
+		glTexStorage2D(GL_TEXTURE_2D, 1, GL_DEPTH_COMPONENT16, width, height);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+		float borderColor[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+		glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+		glBindFramebuffer(GL_FRAMEBUFFER, framebuffer.fbo);
+		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, framebuffer.depthBuffer, 0);
+
+
+
+
+		//TODO: Check for completeness
+
+		GLenum fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+
+		if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
+		{
+			printf("Not complete");
+		}
+
+
+
+
+
+		//Clean up global state
+		glBindTexture(GL_TEXTURE_2D, 0);
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		return framebuffer;
+	}
 
 
 }
